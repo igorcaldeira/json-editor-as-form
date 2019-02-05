b44e715b27f9ef80932390ba5c79c8efc0832840
igoorcaldeira <igorcaldeira@ioasys.co>
2019-02-05 15:13:51 -0200

    Updates

---
+++ precache-manifest.e3f6768c1c9ad681586ae612c5ccd234.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e791881c1861ed393648",
+    "url": "/json-editor-as-form/static/js/runtime~main.e791881c.js"
+  },
+  {
+    "revision": "ce824c56570b4d9b0010",
+    "url": "/json-editor-as-form/static/js/main.ce824c56.chunk.js"
+  },
+  {
+    "revision": "26a37eb0ccd1fc6601b7",
+    "url": "/json-editor-as-form/static/js/1.26a37eb0.chunk.js"
+  },
+  {
+    "revision": "ce824c56570b4d9b0010",
+    "url": "/json-editor-as-form/static/css/main.370cb598.chunk.css"
+  },
+  {
+    "revision": "ebcf299f6313398b766f31473845db00",
+    "url": "/json-editor-as-form/index.html"
+  }
+];
